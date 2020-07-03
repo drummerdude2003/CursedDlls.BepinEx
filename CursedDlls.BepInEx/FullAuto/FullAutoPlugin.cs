5a30659492a8d04a55e7f1174395a45ecf5e719e
ghorsington <neigh@coder.horse>
2020-07-03 21:56:26 +0300

    Clean up solution

---
+++ CursedDlls.BepInEx/FullAuto/FullAutoPlugin.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+
+namespace Cursed.FullAuto
+{
+    [BepInPlugin("com.drummerdude2003.curseddll", "Full Auto", "1.0")]
+    public class FullAutoPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Hooks.InstallHooks();
+        }
+    }
+}
