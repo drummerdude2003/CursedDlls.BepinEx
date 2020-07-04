19a90cc2bd67965dbb10cfe0bd507de0a324fa5f
ghorsington <neigh@coder.horse>
2020-07-04 12:54:32 +0300

    Add initial time scale fix

---
+++ CursedDlls.BepInEx/Cursed.TimeScale/TimeScaleFixPlugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Cursed.TimeScale
+{
+    [BepInPlugin("dll.cursed.timescalefix", "Time scaler", "1.0")]
+    public class TimeScaleFixPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(TimeScaleFixPlugin));
+        }
+
+        [HarmonyPatch(typeof(AudioSource), "pitch", MethodType.Setter)]
+        [HarmonyPrefix]
+        public static void FixPitch(ref float value)
+        {
+            Debug.Log("Fixing pitch to match timescale");
+            value *= Time.timeScale;
+        }
+    }
+}
