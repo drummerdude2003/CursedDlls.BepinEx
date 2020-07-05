7ecb3feb95e0d1db58ec013e3d70a94be0fd9bb7
ghorsington <neigh@coder.horse>
2020-07-05 21:51:06 +0300

    Add RemoveAttachmentChecks plugin

---
+++ CursedDlls.BepInEx/Cursed.RemoveAttachmentChecks/RemoveAttachmentChecksPlugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using FistVR;
+using HarmonyLib;
+
+namespace Cursed.RemoveAttachmentChecks
+{
+    [BepInPlugin("dll.cursed.removeattachmentchecks", "Remove attachment checks", "1.0")]
+    public class RemoveAttachmentChecksPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(RemoveAttachmentChecksPlugin));
+        }
+
+        [HarmonyPatch(typeof(FVRFireArmAttachment), "AttachToMount")]
+        [HarmonyPatch(typeof(FVRFireArmAttachment), "GetRotTarget")]
+        [HarmonyPrefix]
+        public static void SetBiDirectional(ref bool ___IsBiDirectional)
+        {
+            ___IsBiDirectional = true;
+        }
+
+        [HarmonyPatch(typeof(FVRFireArmBipod), "UpdateBipod")]
+        [HarmonyPrefix]
+        public static void RemoveBipodRecoil(ref float ___RecoilDamping)
+        {
+            ___RecoilDamping = 0f;
+        }
+
+        [HarmonyPatch(typeof(FVRFireArmAttachmentMount), "Awake")]
+        [HarmonyPostfix]
+        public static void RemoveMaxAttachmentLimit(ref int ___m_maxAttachments)
+        {
+            ___m_maxAttachments = int.MaxValue;
+        }
+    }
+}
--- CursedDlls.BepInEx/Cursed.UnlockAll/UnlockAllPlugin.cs
+++ CursedDlls.BepInEx/Cursed.UnlockAll/UnlockAllPlugin.cs
@@ -21,39 +21,42 @@ namespace Cursed.UnlockAll
         public static IEnumerable<CodeInstruction> GenerateItemDBsTranspiler(IEnumerable<CodeInstruction> instrs)
         {
             return new CodeMatcher(instrs)
-                .MatchForward(true, 
+                .MatchForward(true,
                     new CodeMatch(OpCodes.Ldstr, "ItemSpawnerIDs"),
                     new CodeMatch(OpCodes.Call))
                 .Advance(1)
                 .Insert(
                     new CodeInstruction(OpCodes.Ldloc_0),
-                    Transpilers.EmitDelegate<Func<ItemSpawnerID[], FVRObject[], ItemSpawnerID[]>>((spawnerIds, fvrObjects) =>
-                    {
-                        var objects = new List<FVRObject>(fvrObjects);
-                        objects.Reverse();
-                        var extSpawnerIds = new List<ItemSpawnerID>(spawnerIds);
-                        foreach (var itemSpawnerId in spawnerIds)
+                    Transpilers.EmitDelegate<Func<ItemSpawnerID[], FVRObject[], ItemSpawnerID[]>>(
+                        (spawnerIds, fvrObjects) =>
                         {
-                            if (itemSpawnerId.MainObject != null)
-                                objects.Remove(itemSpawnerId.MainObject);
-                            if (itemSpawnerId.SecondObject != null)
-                                objects.Remove(itemSpawnerId.SecondObject);
-                        }
-                        foreach (var fvrObject in objects)
-                        {
-                            var itemId = ScriptableObject.CreateInstance<ItemSpawnerID>();
-                            itemId.DisplayName = fvrObject.DisplayName;
-                            itemId.SubHeading = fvrObject.ItemID;
-                            itemId.Category = ItemSpawnerID.EItemCategory.Misc;
-                            itemId.SubCategory = ItemSpawnerID.ESubCategory.Backpack;
-                            itemId.ItemID = fvrObject.ItemID;
-                            itemId.MainObject = fvrObject;
-                            itemId.Secondaries = new ItemSpawnerID[0];    
-                            itemId.UsesHugeSpawnPad = true;
-                            extSpawnerIds.Add(itemId);
-                        }
-                        return extSpawnerIds.ToArray();
-                    }))
+                            var objects = new List<FVRObject>(fvrObjects);
+                            objects.Reverse();
+                            var extSpawnerIds = new List<ItemSpawnerID>(spawnerIds);
+                            foreach (var itemSpawnerId in spawnerIds)
+                            {
+                                if (itemSpawnerId.MainObject != null)
+                                    objects.Remove(itemSpawnerId.MainObject);
+                                if (itemSpawnerId.SecondObject != null)
+                                    objects.Remove(itemSpawnerId.SecondObject);
+                            }
+
+                            foreach (var fvrObject in objects)
+                            {
+                                var itemId = ScriptableObject.CreateInstance<ItemSpawnerID>();
+                                itemId.DisplayName = fvrObject.DisplayName;
+                                itemId.SubHeading = fvrObject.ItemID;
+                                itemId.Category = ItemSpawnerID.EItemCategory.Misc;
+                                itemId.SubCategory = ItemSpawnerID.ESubCategory.Backpack;
+                                itemId.ItemID = fvrObject.ItemID;
+                                itemId.MainObject = fvrObject;
+                                itemId.Secondaries = new ItemSpawnerID[0];
+                                itemId.UsesHugeSpawnPad = true;
+                                extSpawnerIds.Add(itemId);
+                            }
+
+                            return extSpawnerIds.ToArray();
+                        }))
                 .InstructionEnumeration();
         }
 
@@ -65,12 +68,15 @@ namespace Cursed.UnlockAll
             {
                 __instance.Rewards.Add(ID);
                 using (var writer = ES2Writer.Create("Rewards.txt"))
-                    __instance.SaveToFile(writer);    
+                {
+                    __instance.SaveToFile(writer);
+                }
             }
+
             __result = true;
             return false;
         }
-        
+
         [HarmonyPatch(typeof(RewardUnlocks), nameof(RewardUnlocks.IsRewardUnlocked), typeof(ItemSpawnerID))]
         [HarmonyPrefix]
         public static bool IsRewardUnlockedPrefix(RewardUnlocks __instance, ref bool __result, ItemSpawnerID ID)
