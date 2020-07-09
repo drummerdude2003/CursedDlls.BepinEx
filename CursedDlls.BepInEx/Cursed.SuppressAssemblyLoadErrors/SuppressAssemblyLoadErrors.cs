902a26dd6423fde819b324d7619b10b861c8f710
ghorsington <neigh@coder.horse>
2020-07-09 21:17:33 +0300

    Add SuppressAssemblyLoadErrors plugin

---
+++ CursedDlls.BepInEx/Cursed.SuppressAssemblyLoadErrors/SuppressAssemblyLoadErrors.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Cursed.SuppressAssemblyLoadErrors
+{
+    [BepInPlugin("dll.cursed.suppressassemblyloaderrors", "Suppress assembly load errors", "1.0")]
+    public class SuppressAssemblyLoadErrors : BaseUnityPlugin
+    {
+        internal static ManualLogSource logger; 
+            
+        private void Awake()
+        {
+            logger = Logger;
+            Harmony.CreateAndPatchAll(typeof(SuppressAssemblyLoadErrors));
+        }
+
+        [HarmonyPatch(typeof(Assembly), nameof(Assembly.GetTypes), new Type[0])]
+        [HarmonyFinalizer]
+        public static void HandleReflectionTypeLoad(ref Exception __exception, ref Type[] __result)
+        {
+            if (__exception == null)
+                return;
+            if (__exception is ReflectionTypeLoadException re)
+            {
+                __exception = null;
+                __result = re.Types;
+                logger.LogDebug($"Encountered ReflectionTypeLoadException which was suppressed. Full error: \n${TypeLoader.TypeLoadExceptionToString(re)}");
+            }
+        }
+    }
+}
